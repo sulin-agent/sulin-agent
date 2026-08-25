14e8ca305b5fa8ff09abd0ef6c3ed3013049a49b
sulin-agent <819384057@qq.com>
2026-08-25 15:00:37 +0800

    sync: update day6.py

---
+++ day6.py
@@ -0,0 +1,20 @@
+def trim(s):
+    while len(s) > 0 and s[0] == ' ':
+        s = s[1: ]
+    while len(s) > 0 and s[-1] == ' ':
+        s = s[ :-1]
+    return s
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
